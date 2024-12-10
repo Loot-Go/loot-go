a0e1a4952411dfd11fdb87ddd5c72b55ca9add24
Abil Savio <abilsavio@gmail.com>
2024-12-10 08:08:06 +0530

    feat: add lootgo contracts and airdrop

--- src/app/chest/actions.ts
+++ src/app/chest/actions.ts
@@ -1,14 +1,29 @@
 "use server";
 
+import axios from "axios";
+
+const price = 0.001711;
 export const getRandomAirdrop = async (walletAddress: string) => {
   if (!walletAddress) {
     return null;
   }
+  const randomNumber = Math.floor(Math.random() * 1000000);
+
+  const response = await axios.post(
+    `${process.env.NEXT_PUBLIC_BASE_URI}/api/send_token`,
+    {
+      walletAddress,
+      randomNumber,
+    },
+  );
+
+  const data = await response.data;
+
+  console.log(data);
 
   return {
-    value: "13000",
-    amount: "1790",
-    image:
-      "https://assets.coingecko.com/coins/images/39251/standard/miggles.jpg?1721283044",
+    value: randomNumber ? Number(randomNumber) : 0,
+    amount: randomNumber ? Number(randomNumber) * price : 0,
+    image: "/images/token1.svg",
   };
 };
